dcb21a538878bf93a11aa93cb66139e0a0d61e16
Aanand Prasad <aanand.prasad@gmail.com>
2012-09-04 16:54:08 +0100

    Even more documentation

--- medallion.js
+++ medallion.js
@@ -13,6 +13,24 @@
  * - text: Set to false to stop showing the 'My current jam is:' tag.
  * - image: Set to false to stop showing the jam avatar.
  * - imageSize: "small" (default), "medium" or "large".
+ *
+ * Markup
+ * ------
+ *
+ * The generated markup will look something like this:
+ *
+ * <div class="jam-medallion">
+ *   <a class="jam-image" href="...">
+ *     <img class="jam-jamvatar" src="...">
+ *   </a>
+ *
+ *   <p class="jam-text">
+ *     My current jam is <a href="...">...</a>.
+ *   </p>
+ * </div>
+ *
+ * A "jam-loading" class is added to the outer div while the jam data is being
+ * loaded from the API, so that you can style it appropriately.
  */
 
 // Setup + utilities
